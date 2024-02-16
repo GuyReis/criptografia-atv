cfa1613129eb5f7b60b671d56e7024594495246d
Nicholas <nicholasreis001@gmail.com>
2024-02-16 11:00:35 -0300

    mudanças na forma de criptografar o texto

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-let textoResultado = document.getElementById('textoResultado').innerHTML
-let inputTexto = document.getElementById('inputTexto')
 
+let palavrasCripto = {
+    "a": "ai",
+    "e" : "enter",
+    "i" : "imes", 
+    "o" : "ober",
+    "u" : "ufat"
+};
+let palavrasDescripto ={
+    "ai" : "a",
+    "enter" : "e",
+    "imes" : "i", 
+    "ober" : "o",
+    "ufat" : "u"
+};
 
 function checarInput(){
     let inputValor = document.getElementById('inputTexto').value;
@@ -10,61 +22,58 @@ function checarInput(){
         botaoCripto.disabled = false;
         botaoDescri.disabled = false;
         botaoCripto.classList.remove('disabled');
-        botaoDescri.classList.remove('disabled')
+        botaoDescri.classList.remove('disabled');
 
     } 
     else{
         botaoCripto.disabled = true;
         botaoDescri.disabled = true;
         botaoCripto.classList.add('disabled');
-        botaoDescri.classList.add('disabled')
+        botaoDescri.classList.add('disabled');
 
     }
 
 }
 
-
-function criptografarTexto(){
-    let resultado = document.getElementById('textoResultado');
-    let botaoCopiar = document.querySelector('#botaoCopiar');
-
-    inputValor = document.getElementById('inputTexto').value.toLowerCase();
-    inputValor = inputValor.replace(/e/g, 'enter');
-    inputValor = inputValor.replace(/i/g, 'imes');
-    inputValor = inputValor.replace(/a/g, 'ai');
-    inputValor = inputValor.replace(/o/g, 'ober');
-    inputValor = inputValor.replace(/u/g, 'ufat');
-    
-    resultado.innerHTML = inputValor;
-
-    botaoCopiar.addEventListener('click',()=>{
-    navigator.clipboard.writeText(inputValor);
-
-})
-
-
-
-   }
-
-   function descriptografarTexto(){
-
-    let resultado = document.getElementById('textoResultado');
-    let botaoCopiar = document.querySelector('#botaoCopiar');
-
-    inputValor = document.getElementById('inputTexto').value.toLowerCase();
-    inputValor = inputValor.replace(/enter/g, 'e');
-    inputValor = inputValor.replace(/imes/g, 'i');
-    inputValor = inputValor.replace(/ai/g, 'a');
-    inputValor = inputValor.replace(/ober/g, 'o');
-    inputValor = inputValor.replace(/ufat/g, 'u');
-    
-    resultado.innerHTML = inputValor;
-    botaoCopiar.addEventListener('click',()=>{
-    navigator.clipboard.writeText(inputValor);
-
-   })
+function criptografarTexto(texto){
+        
+        let input = new RegExp(Object.keys(palavrasCripto).join("|"), "gi");       
+        function criptografar(match) {
+            return palavrasCripto[match.toLowerCase()];
+        }       
+        let textoCriptografado = texto.replace(input, criptografar);
+        return textoCriptografado;
 }
 
+function criptografarInput(){
+     let resultado = document.getElementById('textoResultado');
+     let inputTexto = document.getElementById('inputTexto');
+     let textoOriginal = inputTexto.value.toLowerCase();
+     let textoCripto = criptografarTexto(textoOriginal);
+    resultado.innerHTML = textoCripto;
+}
+
+   function descriptografarTexto(texto){
+    let input = new RegExp(Object.keys(palavrasDescripto).join("|"), "gi");
+        function descriptografar(match) {
+            return palavrasDescripto[match.toLowerCase()];
+        }       
+        let textoDescriptografado = texto.replace(input, descriptografar);
+        return textoDescriptografado;
+}
+
+function descriptografarInput(){
+    let resultado = document.getElementById('textoResultado');
+    let inputTexto = document.getElementById('inputTexto');
+    let textoOriginal = inputTexto.value.toLowerCase();
+    let textoDescri = descriptografarTexto(textoOriginal);
+    resultado.innerHTML = textoDescri;
+}
+
+function copiarResultado(){
+    
+
+}
    
    
    
